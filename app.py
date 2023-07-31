1630681b087c6f08b00cd44ba1080caf3e25b5a7
harper <haipengchan@gmail.com>
2023-07-31 15:22:09 +0000

    ..

--- app.py
+++ app.py
@@ -12,3 +12,20 @@ def db_testing():
     conn = psycopg2.connect("postgres://blackjack007h_user:u1uVis0w9op35qYYRVBSm8GlVt3UpkFc@dpg-cj3sj0h8g3n1jkk4pc4g-a/blackjack007h")
     conn.close()
     return "Database connection works properly!"
+
+@app.route('/db_create')
+def db_create():
+    conn = psycopg2.connect("postgres://blackjack007h_user:u1uVis0w9op35qYYRVBSm8GlVt3UpkFc@dpg-cj3sj0h8g3n1jkk4pc4g-a/blackjack007h")
+    cur = conn.cursor()
+    cur.execute('''
+        CREATE TABLE IF NOT EXISTS Basketball(
+            First varchar(255),
+            Last varchar(255),
+            City varchar(255),
+            Name varchar(255),
+            Number int
+            );
+    ''')
+    conn.commit()
+    conn.close()
+    return "Table Basketball was successfully created!"
