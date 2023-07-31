392113987f1787d2623eb55481b034a1a114ed1e
harper <haipengchan@gmail.com>
2023-07-31 15:51:24 +0000

    ...

--- app.py
+++ app.py
@@ -49,7 +49,6 @@ def db_insert():
 @app.route('/db_select')
 def selecting():
     conn = psycopg2.connect("postgres://blackjack007h_user:u1uVis0w9op35qYYRVBSm8GlVt3UpkFc@dpg-cj3sj0h8g3n1jkk4pc4g-a/blackjack007h") 
-    connection string
     cur = conn.cursor()
     cur.execute('SELECT * FROM Basketball;')  
     records = cur.fetchall()
